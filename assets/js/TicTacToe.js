ca418fe9f2a8a1186d12d22635191bb6287d25de
Yasser Abdolmaleki <yasser@yasser.ca>
2015-01-10 20:17:39 -0500

    Horizontal Win Detects

--- assets/js/TicTacToe.js
+++ assets/js/TicTacToe.js
@@ -1,33 +1,88 @@
 var TicTacToe = function () {
 
-    var EMPTY = null;
-
-    var board = [
+    var emptyBoard = [
     	[null,null,null],
     	[null,null,null],
     	[null,null,null],
     ];
 
+    var board = emptyBoard;
+    var isGameOver = false;
+    var winner = null;
+    var moves = 0;
+    var winnerLineType = null;
+    var winnerLineNumber = null;
+
     var play = function (player, cell){
+        if(isGameOver === true){
+            throw new exceptions.PlayingNotPossible();
+        }
         shape = getPlayerShape(player);
-        changeCell(cell ,shape)
+        changeCell(cell ,shape);
+        discoverAWin();
     }
 
     var getPlayerShape = function(player){
-        if(player == 1)
-            return 'x'
-        else
-            return 'o'
+        if(player == 1){
+            return 'x';
+        }else{
+            return 'o';
+        }
+    }
+
+    var getPlayerFromShape = function(player){
+        if(player == 'x'){
+            return 1;
+        }else{
+            return 2;
+        }
     }
 
     var changeCell = function(cell, shape){
         var row = cell[0]-1;
         var col = cell[1]-1;
-        board[row, col] = 1;
+        board[row][col] = shape;
+        moves++;
+    }
+
+    var discoverAWin = function(){
+       discoverHorizontalWin();
+    }
+
+    var discoverHorizontalWin = function(){
+        for(var row = 0; row < 3; row++){
+            if(board[row][0] != null && board[row][0] == board[row][1] && board[row][0]  == board[row][2]){
+                finilizeWin('horizontal', row + 1, getPlayerFromShape(board[0][0]));
+            }
+        }
+    }
+
+    var finilizeWin = function(lineType, lineNumber, player){
+        winnerLineType = lineType;
+        winnerLineNumber = lineNumber;
+        winner = player;
+        isGameOver = true;
+    }
+
+    var getIsGameOver = function(){
+        return isGameOver;
+    }
+
+    var getWinner = function(){
+        return winner;
+    }
+
+    var exceptions = {
+        PlayingNotPossible : function(){
+            throw new Error("Playing is not possible, game is over");
+        }
     }
 
     return {
         play: play,
-        board : board
+        board : board,
+        getIsGameOver : getIsGameOver,
+        getWinner : getWinner,
+        exceptions : exceptions
     }
 };
--- jasmine/spec/ticTacToeSpec.js
+++ jasmine/spec/ticTacToeSpec.js
@@ -2,7 +2,7 @@ describe("TicTacToe", function() {
   var ticTacToe;
 
   beforeEach(function() {
-    ticTacToe = new TicTacToe;
+    ticTacToe = new TicTacToe();
     emptyBoard = [ [ null, null, null ], [ null, null, null ], [ null, null, null ] ] ;
   });
 
@@ -10,16 +10,64 @@ describe("TicTacToe", function() {
     expect(ticTacToe.board).toEqual(emptyBoard);
   });
 
-  describe("When first move has been made", function() {
+  it("Game should not be over in a new game", function() {
+    expect(ticTacToe.isGameOver).toBeFalsy();
+  });
 
+  describe("When first move has been made", function() {
     beforeEach(function() {
       ticTacToe.play(1, [1,1]);
     });
 
     it("one change has happened to the board", function() {
-      expect(ticTacToe.board).not.toEqual(emptyBoard);
+      changedBoard = [ [ 'x', null, null ], [ null, null, null ], [ null, null, null ] ] ;
+      expect(ticTacToe.board).toEqual(changedBoard);
+    });
+
+    it("Game should not be over after the first game", function() {
+      expect(ticTacToe.isGameOver).toBeFalsy();
+    });
+
+    it("Game should not have a winner after the first game", function() {
+      expect(ticTacToe.getWinner()).toEqual(null);
     });
   });
 
+  describe("When a player wins", function() {
+    beforeEach(function() {
+      ticTacToe = new TicTacToe();
+    });
+
+    it("Game is over with Horizontal win", function() {
+      ticTacToe.play(1, [1,1]);
+      ticTacToe.play(2, [2,1]);
+      ticTacToe.play(1, [1,3]);
+      ticTacToe.play(2, [2,2]);
+      ticTacToe.play(1, [1,2]);
+      expect(ticTacToe.getIsGameOver()).toBeTruthy();
+    });
+
+    it("Game can not be played anymore", function() {
+      ticTacToe.play(1, [1,1]);
+      ticTacToe.play(2, [2,1]);
+      ticTacToe.play(1, [1,3]);
+      ticTacToe.play(2, [2,2]);
+      ticTacToe.play(1, [1,2]);
+      expect( function(){ 
+        ticTacToe.play(1, [1,1]);
+      }).toThrow(
+        new Error("Playing is not possible, game is over")
+      );
+    });
+
+    it("There's a winner", function() {
+      ticTacToe.play(1, [1,1]);
+      ticTacToe.play(2, [2,1]);
+      ticTacToe.play(1, [1,3]);
+      ticTacToe.play(2, [2,2]);
+      ticTacToe.play(1, [1,2]);
+      expect(ticTacToe.getWinner()).not.toEqual(null);
+    });
+  });
 
 });
