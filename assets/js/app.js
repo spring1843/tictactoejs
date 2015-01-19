e2912c172a2fd356e59c31eba09ff04787563558
Yasser Abdolmaleki <yasser@yasser.ca>
2015-01-18 21:58:40 -0500

    Game can be played

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,17 +4,49 @@
         .controller('MainController', ['$scope', MainController]);
 
     function MainController($scope) {
-        $scope.board = [
-            [null, null, null],
-            [null, null, null],
-            [null, null, null],
-        ];
+
+
+        $scope.ticTacToe = new TicTacToe();
+        $scope.player1 = new Player($scope.ticTacToe, 1);
+        $scope.player2 = new Player($scope.ticTacToe, 2);
+        $scope.gameStatus = $scope.ticTacToe.getGameStatus();
+        $scope.board = $scope.ticTacToe.getBoard();
 
         $scope.getCellShape = function(boardCell){
             if(boardCell === null)
                 return '+';
             else
                 return boardCell;
+        }
+
+        $scope.refreshStatus = function(){
+            var gameStatus = $scope.ticTacToe.getGameStatus();
+            $scope.gameStatus = gameStatus;
+            return gameStatus;
+        }
+
+        var canPlayerPlay = function(boardCell){
+            if($scope.gameStatus.isGameOver === true)
+                return false;
+
+            if($scope.board[boardCell[0]-1][boardCell[1]-1] != null)
+                return false;
+
+            return true;
+        }
+
+        $scope.playForUser = function(boardCell){
+            if(canPlayerPlay(boardCell) === false)
+                return;
+
+            $scope.player1.play(boardCell[0], boardCell[1]);
+            $scope.refreshStatus();
+
+            if($scope.refreshStatus().isGameOver === false)
+                $scope.player2.autoPlay();
+
+            $scope.board = $scope.ticTacToe.getBoard();
+            $scope.refreshStatus();
         }
 
     }
